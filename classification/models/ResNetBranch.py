bf9f14232fe5584e9e4e56043237b783dde24d83
renqian <778315181@qq.com>
2020-11-02 10:05:00 +0800

    add branch

---
+++ classification/models/ResNetBranch.py
@@ -0,0 +1,181 @@
+"""Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+
+Portions of the source code are from the OLTR project which
+notice below and in LICENSE in the root directory of
+this source tree.
+
+Copyright (c) 2019, Zhongqi Miao
+All rights reserved.
+"""
+
+
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+        
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+    
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, strides=[1,2,2,2],branch=1,depth=0):
+        inplanes = 64
+        planes=[64,128,256,512]
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.share_layers, in_planes = self.generate_share(block, in_planes,planes, strides, num_blocks, depth)
+        self.branchs = nn.ModuleList([ self.generate_branch(block, in_planes,planes, strides, num_blocks, depth) for i in range(branch)])
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        
+        self.apply(_weights_init)
+    def generate_branch(self, block, in_planes, planes,strides, num_blocks, depth):
+        branch_layers = []
+        set_branch = False
+        all_blocks = sum(num_blocks)
+        for i,num_block in enumerate(num_blocks):
+            if all_blocks - depth<num_block:
+                set_branch = True
+                branch_layers.append(self._make_layer(block,in_planes,planes[i],depth+num_block-all_blocks,stride=1))
+                in_planes = planes[i]*block.expansion
+            elif set_branch:
+                branch_layers.append(self._make_layer(block,in_planes,planes[i],num_block,stride=strides[i]))
+                in_planes = planes[i]*block.expansion
+            all_blocks -= num_block
+        return nn.Sequential(*branch_layers)
+    
+    def generate_share(self, block, in_planes,planes,strides, num_blocks, depth):
+        share_layers = []
+        all_blocks = sum(num_blocks)
+        for i,num_block in enumerate(num_blocks):
+            if all_blocks - depth<num_block:
+                share_layers.append(self._make_layer(block,in_planes,planes[i],all_blocks-depth,stride=strides[i]))
+                break
+            else:
+                share_layers.append(self._make_layer(block,in_planes,planes[i],num_block,stride=strides[i]))
+            in_planes=planes[i]*block.expansion
+            all_blocks -= num_block
+
+        return nn.Sequential(*share_layers),planes[i]
+
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x, *args):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.share_layers(x)
+        if len(self.branchs)>0:
+            x = torch.cat([branch(x) for branch in self.branchs], dim=1)
+
+        x = self.avgpool(x)
+        
+        x = x.view(x.size(0), -1)
+        
+
+
+        return x
